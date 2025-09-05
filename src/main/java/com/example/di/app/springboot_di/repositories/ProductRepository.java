c0b7dd2de7e492179ca5add21774b9c6e4a09fce
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-09-05 00:06:03 -0300

    Implementando método findById en la clase Repository

--- src/main/java/com/example/di/app/springboot_di/repositories/ProductRepository.java
+++ src/main/java/com/example/di/app/springboot_di/repositories/ProductRepository.java
@@ -11,16 +11,22 @@ public class ProductRepository {
 
     public ProductRepository() {
         this.products = new ArrayList<>(
-            List.of(
-                new Product(1L, "Monitor", 200.0),
-                new Product(2L, "Teléfono", 100.0),
-                new Product(3L, "Tablet", 300.0),
-                new Product(4L, "Laptop", 800.0)
-            )
-        );
+                List.of(
+                        new Product(1L, "Monitor", 200.0),
+                        new Product(2L, "Teléfono", 100.0),
+                        new Product(3L, "Tablet", 300.0),
+                        new Product(4L, "Laptop", 800.0),
+                        new Product(5L, "Smartwatch", 150.0)));
     }
 
     public List<Product> findAll() {
         return products;
+    }
+
+    public Product findById(Long id) {
+        return products.stream() // convierte la lista en un stream
+                .filter(p -> p.getId().equals(id)) // filtra por id
+                .findFirst() // toma el primero que coincida
+                .orElse(null); // si no hay ninguno, retorna null
     }
 }
